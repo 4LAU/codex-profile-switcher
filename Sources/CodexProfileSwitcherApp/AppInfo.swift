91f317a31956f1b20979ac2d8fe6c9226ee49282
Aaron Lau <aaron.4lau@gmail.com>
2026-06-10 19:07:20 +0200

    chore: bump version to 0.3.0

--- Sources/CodexProfileSwitcherApp/AppInfo.swift
+++ Sources/CodexProfileSwitcherApp/AppInfo.swift
@@ -4,6 +4,6 @@ import Foundation
 
 enum AppInfo {
     static let name = "CodexProfileSwitcher"
-    static let version = "0.2.1"
+    static let version = "0.3.0"
     static let issueURL = URL(string: "https://github.com/4LAU/codex-profile-switcher/issues/new")!
 }
