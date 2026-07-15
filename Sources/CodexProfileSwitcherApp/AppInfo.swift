abfc1ade611fc77c26820293a1e0558a85a76514
Aaron Lau <aaron.4lau@gmail.com>
2026-07-15 16:40:28 -0700

    chore: prepare v0.5.11

--- Sources/CodexProfileSwitcherApp/AppInfo.swift
+++ Sources/CodexProfileSwitcherApp/AppInfo.swift
@@ -4,6 +4,6 @@ import Foundation
 
 enum AppInfo {
     static let name = "CodexProfileSwitcher"
-    static let version = "0.5.10"
+    static let version = "0.5.11"
     static let issueURL = URL(string: "https://github.com/4LAU/codex-profile-switcher/issues/new")!
 }
