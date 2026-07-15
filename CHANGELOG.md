755a860cd41118f7f933b095ad4d8763b92d2c8e
Aaron Lau <aaron.4lau@gmail.com>
2026-07-15 13:17:00 -0700

    fix: align release version labels

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/).
 
 ## Unreleased
 
+## 0.5.5 -- 2026-07-15
+
+### Fixed
+
+- The app and bundled helper now report the same version as the signed package.
+
 ## 0.5.4 -- 2026-07-15
 
 ### Fixed
--- Sources/CodexProfileSwitcherApp/AppInfo.swift
+++ Sources/CodexProfileSwitcherApp/AppInfo.swift
@@ -4,6 +4,6 @@ import Foundation
 
 enum AppInfo {
     static let name = "CodexProfileSwitcher"
-    static let version = "0.5.3"
+    static let version = "0.5.5"
     static let issueURL = URL(string: "https://github.com/4LAU/codex-profile-switcher/issues/new")!
 }
